d0bfebb4a9e14aa56e814bfafacf7188b994f10d
Bibek Sabat <bibekreigns5278@gmail.com>
2025-09-06 03:51:17 +0530

    06: e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,18 @@
+import {Sandbox} from "@e2b/code-interpreter"
 import {  gemini, createAgent } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const SandboxId = await step.run("get-sandbox-id" , async () => {
+      const sandbox= await Sandbox.create("whis-nextjs-text-2");
+      return sandbox.sandboxId;
+    });
    
     const codeAgent = createAgent({
   name: "codeAgent",
@@ -16,8 +23,13 @@ export const helloWorld = inngest.createFunction(
 const { output } = await codeAgent.run(
   `Write the following snippts: ${event.data.value} `,
 );
-console.log(output);
+
+  const sandboxUrl = await step.run("get-sandbox-url", async () => {
+    const sandbox = await getSandbox(SandboxId);
+    const host =  sandbox.getHost(3000);
+    return `https://${host}`;
+  })
     
-    return { output };
+    return { output, sandboxUrl };
   },
 );
